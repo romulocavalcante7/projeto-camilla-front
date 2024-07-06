bf479cd164e56e92cc28d7b9f6c1f594f40223ea
Wallace-Bezerra <wallace_2014_@hotmail.com>
2024-07-06 09:14:03 -0300

    feat: page recents ajust

--- app/(app)/recentes/page.tsx
+++ app/(app)/recentes/page.tsx
@@ -1,82 +1,14 @@
 'use client';
 
-import { useEffect, useState } from 'react';
-import {
-  getStickersBySubnicheId,
-  Sticker,
-  StickerResponse
-} from '@/services/stickerService';
-import { useRouter, useSearchParams } from 'next/navigation';
-import { ArrowLeft, Loader2 } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import { ArrowLeft } from 'lucide-react';
 import Image from 'next/image';
-import Clipboard from '@/components/clipboard';
-import InfiniteScroll from '@/components/ui/InfiniteScroll';
+
 import Search from '@/components/search';
 import Link from 'next/link';
 
-interface SubnicheProps {
-  params: {
-    subnicheId: string;
-  };
-}
-
-const RecentStickerList = ({ params }: SubnicheProps) => {
-  const searchParams = useSearchParams();
+const RecentStickerList = () => {
   const router = useRouter();
-  const name = searchParams.get('name');
-  const { subnicheId } = params;
-
-  const [page, setPage] = useState<number>(1);
-  const [loading, setLoading] = useState<boolean>(false);
-  const [hasMore, setHasMore] = useState<boolean>(true);
-  const [stickers, setStickers] = useState<Sticker[]>([]);
-  const [search, setSearch] = useState<string | undefined>(
-    searchParams.get('search') || undefined
-  );
-
-  const fetchStickers = async (page: number, search?: string) => {
-    setLoading(true);
-    try {
-      const data: StickerResponse = await getStickersBySubnicheId(
-        subnicheId,
-        page,
-        10,
-        search
-      );
-      setStickers((prev) => {
-        const newStickers = data.stickers.filter(
-          (sticker) =>
-            !prev.some((prevSticker) => prevSticker.id === sticker.id)
-        );
-        return [...prev, ...newStickers];
-      });
-      setHasMore(page < data.totalPages);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchStickers(page, search);
-  }, [page, search]);
-
-  const loadMore = () => {
-    if (!loading) {
-      setTimeout(() => {
-        setPage((prevPage) => prevPage + 1);
-      }, 1000);
-    }
-  };
-
-  const handleSearch = (newSearch: string) => {
-    if (!newSearch) {
-      return setSearch(newSearch);
-    }
-    setSearch(newSearch);
-    setPage(1);
-    setStickers([]);
-  };
-
   return (
     <div className="flex w-full flex-col gap-3">
       <div className="sticky left-0 top-0 z-10 flex w-full flex-col gap-5 bg-white py-5 transition-all">
@@ -102,33 +34,16 @@ const RecentStickerList = ({ params }: SubnicheProps) => {
               size={30}
               onClick={() => router.back()}
             />
-            <p className="text-2xl font-bold">{name}</p>
+            <p className="text-2xl font-bold">Recentes</p>
           </div>
         </div>
         <Search
-          onSearch={handleSearch}
+          onSearch={() => {}}
           placeholder="Busque uma figurinha"
-          defaultValues={{ search }}
+          defaultValues={{ search: '' }}
         />
       </div>
-      <div className="max-h-full w-full overflow-y-auto">
-        <div className="flex w-full flex-col items-center gap-3">
-          <Clipboard stickers={stickers} />
-          <InfiniteScroll
-            hasMore={hasMore}
-            isLoading={loading}
-            next={loadMore}
-            threshold={1}
-          >
-            {hasMore && <Loader2 className="my-4 h-8 w-8 animate-spin" />}
-          </InfiniteScroll>
-          {!hasMore && stickers.length === 0 && (
-            <p className="text-xl text-gray-500">
-              Não possui figurinhas nesse subnicho
-            </p>
-          )}
-        </div>
-      </div>
+      <p>Recentes ...</p>
     </div>
   );
 };
