f4593b8df40aff5c4ecf2f49fa98aea9de5c8cc7
Wallace-Bezerra <wallace_2014_@hotmail.com>
2024-07-06 10:24:05 -0300

    feat: hidden login page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -12,8 +12,8 @@ export const metadata: Metadata = {
 
 export default function AuthenticationPage() {
   return (
-    <div className="relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-      <Link
+    <div className="relative h-screen flex-col items-center justify-center lg:px-0">
+      {/* <Link
         href="/examples/authentication"
         className={cn(
           buttonVariants({ variant: 'ghost' }),
@@ -22,7 +22,7 @@ export default function AuthenticationPage() {
       >
         Login
       </Link>
-      <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
+      <div className="relative hidden h-full flex-col bg-muted p-10 text-white  dark:border-r">
         <div className="absolute inset-0 bg-zinc-900" />
         <div className="relative z-20 flex items-center text-lg font-medium">
           <svg
@@ -49,7 +49,7 @@ export default function AuthenticationPage() {
             <footer className="text-sm">Sofia Davis</footer>
           </blockquote>
         </div>
-      </div>
+      </div> */}
       <div className="flex h-full items-center p-4 lg:p-8">
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
           <div className="flex flex-col items-center gap-5 text-center">
