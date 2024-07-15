493582df8b40614c1937e0fe9f88bfe82bd6f252
Wallace-Bezerra <wallace_2014_@hotmail.com>
2024-07-15 09:48:32 -0300

    feat: mobile menu

--- components/layout/mobile-sidebar.tsx
+++ components/layout/mobile-sidebar.tsx
@@ -3,7 +3,10 @@ import { DashboardNav } from '@/components/dashboard-nav';
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
 import { navItems } from '@/constants/data';
 import { MenuIcon } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
 import { useState } from 'react';
+import Logo from '@/public/logo-v2.png';
 
 // import { Playlist } from "../data/playlists";
 
@@ -22,6 +25,15 @@ export function MobileSidebar({ className }: SidebarProps) {
         <SheetContent side="left" className="!px-0">
           <div className="space-y-4 py-4">
             <div className="px-3 py-2">
+              <Link className="" href="/">
+                <Image
+                  className="w-32 px-3"
+                  src={Logo}
+                  width={800}
+                  height={600}
+                  alt="logo"
+                />
+              </Link>
               <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">
                 Overview
               </h2>
