8c4d7e769ef1b590653d141d54b41848da2d73ba
Wallace-Bezerra <wallace_2014_@hotmail.com>
2024-07-19 19:42:54 -0300

    bem vinda mensagem

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -20,7 +20,7 @@ export default function page() {
       <div className="flex-1 space-y-4 p-4 pt-6 md:p-8">
         <div className="flex items-center justify-between space-y-2">
           <h2 className="text-3xl font-bold tracking-tight">
-            Olá, Bem Vindo novamente 👋
+            Olá, Bem Vinda Novamente 👋
           </h2>
           {/* <div className="hidden items-center space-x-2 md:flex">
             <CalendarDateRangePicker />
