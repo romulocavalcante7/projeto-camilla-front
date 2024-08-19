05e7051d30cbe53cccc58c4bfca343b89bae3151
Wallace-Bezerra <89711908+Wallace-Bezerra@users.noreply.github.com>
2024-08-19 17:57:03 -0300

    add assinatura page

---
+++ app/(app)/perfil/assinatura/page.tsx
@@ -0,0 +1,105 @@
+'use client';
+
+import { ArrowLeft } from 'lucide-react';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
+import { useScroll, motion } from 'framer-motion';
+import { useContext, useEffect, useState } from 'react';
+import { cn } from '@/lib/utils';
+import AuthContext from '@/contexts/auth-context';
+import { PaymentStatusEnum } from '@/services/types/entities';
+import { getUserPaymentStatus, PaymentStatus } from '@/services/paymentService';
+import { Badge } from '@/components/ui/badge';
+
+const Signature = () => {
+  const router = useRouter();
+  const { scrollY } = useScroll();
+  const [scrollAbove10, setScrollAbove10] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [paymentStatus, setPaymentStatus] = useState<PaymentStatus | null>(
+    null
+  );
+  const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    if (user?.id && user.subscription?.status) {
+      fetchPaymentStatus(user.id);
+    }
+  }, [user?.id, user?.subscription?.status]);
+
+  useEffect(() => {
+    const unsubscribe = scrollY.onChange((latest) => {
+      setScrollAbove10(latest > 10);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, [scrollY]);
+
+  const fetchPaymentStatus = async (id: string) => {
+    try {
+      const data = await getUserPaymentStatus(id);
+      setPaymentStatus(data[0]);
+    } catch (err) {
+      console.log('Failed to fetch payment status');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="flex flex-col gap-2">
+      <motion.div
+        initial={{ opacity: 0, y: 0 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          type: 'tween',
+          duration: 0.4,
+          ease: [0.25, 0.25, 0.25, 0.75]
+        }}
+        className={cn(
+          'sticky left-0 top-0 z-[2] flex w-full flex-col gap-5 bg-white px-5 py-5 transition-all dark:bg-transparent',
+          scrollAbove10 && 'dark:bg-[#1a101b]/80 dark:backdrop-blur-md'
+        )}
+      >
+        <div className="relative flex flex-col gap-2">
+          <Link className="w-fit" href="/">
+            <Image
+              src="/logo-v2.png"
+              width={160}
+              height={40}
+              alt="icone logo"
+            />
+          </Link>
+          <div className="flex items-center gap-5">
+            <ArrowLeft
+              className="cursor-pointer"
+              size={30}
+              onClick={() => router.back()}
+            />
+          </div>
+        </div>
+      </motion.div>
+
+      <div className="flex w-full max-w-lg flex-col  gap-3 px-5">
+        <p className="text-xl">Minha assinatura</p>
+        <div className="flex w-fit gap-5">
+          {paymentStatus && user?.orderStatus === PaymentStatusEnum.Paid && (
+            <>
+              <Badge className="py-1 text-base" variant="default">
+                Status: {paymentStatus.status && 'Ativo'}
+              </Badge>
+              <Badge className="py-1 text-base" variant="secondary">
+                Plano:{' '}
+                {paymentStatus.planType === 'monthly' ? 'Mensal' : 'Anual'}
+              </Badge>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signature;
--- app/(app)/perfil/page.tsx
+++ app/(app)/perfil/page.tsx
@@ -9,34 +9,21 @@ import { cn } from '@/lib/utils';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { useContext, useEffect, useState } from 'react';
 import AuthContext from '@/contexts/auth-context';
-import { Badge } from '@/components/ui/badge';
-import { getUserPaymentStatus, PaymentStatus } from '@/services/paymentService';
-import { PaymentStatusEnum } from '@/services/types/entities';
+
+import { Switch } from '@/components/ui/switch';
+import { useTheme } from 'next-themes';
 
 const Perfil = () => {
   const router = useRouter();
   const { user } = useContext(AuthContext);
-
-  const [paymentStatus, setPaymentStatus] = useState<PaymentStatus | null>(
-    null
-  );
-
   const [loading, setLoading] = useState<boolean>(true);
+  const { setTheme, theme } = useTheme();
 
-  useEffect(() => {
-    if (user?.id && user.subscription?.status) {
-      fetchPaymentStatus(user.id);
-    }
-  }, [user?.id, user?.subscription?.status]);
-
-  const fetchPaymentStatus = async (id: string) => {
-    try {
-      const data = await getUserPaymentStatus(id);
-      setPaymentStatus(data[0]);
-    } catch (err) {
-      console.log('Failed to fetch payment status');
-    } finally {
-      setLoading(false);
+  const ChangeToogle = () => {
+    if (theme === 'dark') {
+      setTheme('light');
+    } else {
+      setTheme('dark');
     }
   };
 
@@ -72,43 +59,57 @@ const Perfil = () => {
           </div>
         </div>
       </motion.div>
-      <div className="flex flex-col items-center justify-center gap-10 px-5">
-        <div className="flex flex-col items-center gap-4">
-          <Avatar className="h-28 w-28">
-            <AvatarImage src={user?.avatar?.url} className="object-cover" />
-            <AvatarFallback className="bg-gray-400" />
-          </Avatar>
-          <p className="text-xl font-semibold">{user?.name}</p>
-          <div className="flex items-center gap-5">
-            {paymentStatus && user?.orderStatus === PaymentStatusEnum.Paid && (
-              <>
-                <Badge className="py-1" variant="default">
-                  Status: {paymentStatus.status && 'Ativo'}
-                </Badge>
-                <Badge className="py-1" variant="outline">
-                  Plano:{' '}
-                  {paymentStatus.planType === 'monthly' ? 'Mensal' : 'Anual'}
-                </Badge>
-              </>
-            )}
+      <div className="flex flex-col items-center justify-center gap-8 px-5">
+        <div className="flex w-full max-w-lg flex-col items-center gap-4">
+          <div className="flex w-full items-center gap-10">
+            <Avatar className="h-20 w-20 sm:h-28 sm:w-28">
+              <AvatarImage src={user?.avatar?.url} className="object-cover" />
+              <AvatarFallback className="bg-gray-400" />
+            </Avatar>
+            <div className="flex flex-col">
+              <p className="text-xl font-semibold sm:text-2xl">{user?.name}</p>
+              <p className="sm:text-lg dark:text-gray-300">{user?.email}</p>
+            </div>
           </div>
         </div>
 
-        <div className="mx-auto flex w-full max-w-lg flex-col gap-5 rounded-lg bg-[#89898938] p-5">
-          <Link
-            href="/perfil/esquecer-senha"
-            className="flex w-full flex-1 cursor-pointer items-center justify-between"
-          >
-            <p>Trocar de senha</p>
-            <ChevronRight />
-          </Link>
-          <Link
-            href="/perfil/avatar"
-            className="flex w-full  flex-1 cursor-pointer items-center justify-between"
-          >
-            <p>Alterar foto de perfil</p>
-            <ChevronRight />
-          </Link>
+        <div className="mx-auto flex w-full max-w-lg flex-col gap-2">
+          <p className="text-xl">Informações</p>
+          <div className="flex flex-col gap-4">
+            <Link
+              href="/perfil/assinatura"
+              className="flex w-full flex-1 cursor-pointer items-center justify-between rounded-xl bg-[#89898938] px-5 py-3"
+            >
+              <p>Minha assinatura</p>
+              <ChevronRight />
+            </Link>
+            <Link
+              href="/perfil/esquecer-senha"
+              className="flex w-full flex-1 cursor-pointer items-center justify-between rounded-xl bg-[#89898938] px-5 py-3"
+            >
+              <p>Trocar de senha</p>
+              <ChevronRight />
+            </Link>
+            <Link
+              href="/perfil/avatar"
+              className="flex w-full flex-1 cursor-pointer items-center justify-between rounded-xl bg-[#89898938] px-5 py-3"
+            >
+              <p>Alterar foto de perfil</p>
+              <ChevronRight />
+            </Link>
+          </div>
+        </div>
+
+        <div className="mx-auto flex w-full max-w-lg flex-col gap-3">
+          <p className="text-xl">Modo de cor</p>
+          <div className="flex items-center gap-3">
+            Escuro
+            <Switch
+              checked={theme === 'light'}
+              onCheckedChange={ChangeToogle}
+            />
+            Claro
+          </div>
         </div>
       </div>
     </div>
