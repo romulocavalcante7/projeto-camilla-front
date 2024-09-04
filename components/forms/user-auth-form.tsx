3a08331c0700cd550349360f81f8effe21d660ca
Wallace-Bezerra <89711908+Wallace-Bezerra@users.noreply.github.com>
2024-09-04 13:54:10 -0300

    link

--- components/forms/user-auth-form.tsx
+++ components/forms/user-auth-form.tsx
@@ -126,7 +126,7 @@ export default function UserAuthForm() {
           </Button>
         </form>
         <Link
-          href="https://pay.kiwify.com.br/Koo7c0u"
+          href="https://www.valquiriabrito.com"
           target="_blank"
           className="mt-8 flex cursor-pointer items-center justify-center text-center font-semibold text-[#AA47B3] sm:text-lg"
         >
