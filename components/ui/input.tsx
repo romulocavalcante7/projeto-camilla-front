3c7c8095360b9751b76a0c16731baea108b5c2a8
Wallace-Bezerra <89711908+Wallace-Bezerra@users.noreply.github.com>
2024-09-07 14:59:42 -0300

    fix input

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,12 +1,6 @@
 import * as React from 'react';
-import { ArrowBigUpDash, EyeIcon, EyeOffIcon, LucideIcon } from 'lucide-react';
+import { EyeIcon, EyeOffIcon, LucideIcon } from 'lucide-react';
 import { cn } from '@/lib/utils';
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger
-} from '@/components/ui/tooltip';
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
@@ -19,14 +13,6 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
     const StartIcon = startIcon;
     const EndIcon = endIcon;
     const [showPassword, setShowPassword] = React.useState(false);
-    const [capsLockActive, setCapsLockActive] = React.useState(false);
-
-    const handleKeyPress: React.KeyboardEventHandler<HTMLInputElement> = (
-      event
-    ) => {
-      const capsLockOn = event.getModifierState('CapsLock');
-      setCapsLockActive(capsLockOn);
-    };
 
     const togglePasswordVisibility = () => setShowPassword(!showPassword);
 
@@ -43,10 +29,10 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
             'flex h-10 w-full rounded-md border border-input bg-background px-4 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50 dark:border-muted-foreground/30 dark:bg-[#323032]/50 dark:text-muted-foreground dark:backdrop-blur-lg',
             startIcon ? 'pl-10' : '',
             endIcon ? 'pr-8' : '',
-            type === 'password' && (!capsLockActive ? 'pr-8' : 'pr-16'),
+            type === 'password' ? 'pr-8' : '',
             className
           )}
-          onKeyDown={handleKeyPress}
+          style={{ fontSize: '16px' }}
           ref={ref}
           {...props}
         />
@@ -57,31 +43,16 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         )}
         {type === 'password' && (
           <div className="absolute right-0 top-1/2 flex -translate-y-1/2 items-center gap-x-1 pr-3">
-            {showPassword ? (
-              <EyeOffIcon
-                className="cursor-pointer text-muted-foreground"
-                onClick={togglePasswordVisibility}
-                size={20}
-              />
-            ) : (
-              <EyeIcon
-                className="cursor-pointer text-muted-foreground"
-                onClick={togglePasswordVisibility}
-                size={20}
-              />
-            )}
-            {capsLockActive && type === 'password' && (
-              <TooltipProvider>
-                <Tooltip>
-                  <TooltipTrigger asChild>
-                    <ArrowBigUpDash size={20} />
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    <p>Caps Lock is on!</p>
-                  </TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
-            )}
+            <div
+              className="cursor-pointer p-2"
+              onClick={togglePasswordVisibility}
+            >
+              {showPassword ? (
+                <EyeOffIcon className="text-muted-foreground" size={20} />
+              ) : (
+                <EyeIcon className="text-muted-foreground" size={20} />
+              )}
+            </div>
           </div>
         )}
       </div>
