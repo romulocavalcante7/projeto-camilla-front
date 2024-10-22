96be06281bc6d473d59722ccd64d52f7a42d0e27
Wallace-Bezerra <wallace_2014_@hotmail.com>
2024-10-22 17:36:32 -0300

    ajust cookies time

--- utils/nextUtils.ts
+++ utils/nextUtils.ts
@@ -3,7 +3,7 @@
 import { cookies } from 'next/headers';
 
 export const setCookie = (name: string, value: string) => {
-  cookies().set(name, value);
+  cookies().set(name, value, { maxAge: 31536000 });
 };
 
 export const getCookie = async (name: string) => {
